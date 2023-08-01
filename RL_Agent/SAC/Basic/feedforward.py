431816cad79f93867b2b3d2ff0d96f96541c53ca
moemchen13 <85173797+moemchen13@users.noreply.github.com>
2023-08-01 14:28:52 +0200

    attempt 14

--- RL_Agent/SAC/Basic/feedforward.py
+++ RL_Agent/SAC/Basic/feedforward.py
@@ -7,18 +7,16 @@ import torch.optim as optim
 
 class Feedforward(torch.nn.Module):
     def __init__(self, input_dim, hidden_sizes, output_size, 
-                 activation_fun=torch.nn.Tanh(), output_activation=None,
-                 name="feedforward",folder="tmp",device='cpu'):
+                 activation_fun=torch.nn.Tanh(),name="feedforward",folder="tmp",device='cpu'):
         super(Feedforward, self).__init__()
         self.device= device
         self.input_size = input_dim
         self.hidden_sizes  = hidden_sizes
         self.output_size  = output_size
-        self.output_activation = output_activation
         layer_sizes = [self.input_size] + self.hidden_sizes
-        self.layers = torch.nn.ModuleList([ torch.nn.Linear(i, o) for i,o in zip(layer_sizes[:-1], layer_sizes[1:])])
+        self.layers = torch.nn.ModuleList([ torch.nn.Linear(i, o) for i,o in zip(layer_sizes[:-1], layer_sizes[1:])]).to(self.device)
         self.activations = [ activation_fun for l in  self.layers ]
-        self.readout = torch.nn.Linear(self.hidden_sizes[-1], self.output_size)
+        self.readout = torch.nn.Linear(self.hidden_sizes[-1], self.output_size).to(self.device)
         self.checkpoint_file = os.path.join(folder,name)
         if self.device == 'cuda':
             self.cuda()
@@ -28,10 +26,8 @@ class Feedforward(torch.nn.Module):
         print(f"forward in feedforward is on cuda: {x.is_cuda}")
         for layer,activation_fun in zip(self.layers, self.activations):
             x = activation_fun(layer(x))
-        if self.output_activation is not None:
-            return self.output_activation(self.readout(x))
-        else:
-            return self.readout(x)
+        print("propagation")
+        return self.readout(x)
     
     def dot_prod_last_layer(self,x,x_prime):
         for layer,activation_fun in zip(self.layers[:-1],self.activations[:-1]):
