bd80735b85682eb97a854d197c3b815f193b6894
moemchen13 <85173797+moemchen13@users.noreply.github.com>
2023-08-02 09:01:45 +0200

    added hockeyfile

---
+++ RL_Agent/PlayGround/hockey.py
@@ -0,0 +1,244 @@
+import optparse
+import pickle
+from importlib import reload
+
+import gymnasium as gym
+import laserhockey.hockey_env as h_env
+import numpy as np
+import pylab as plt
+import torch
+from DSAC.dsac import DSAC_Agent
+from IPython import display
+from SAC.DR3 import DR3_Agent
+from SAC.sac import SAC_Agent
+
+
+def save_statistics(rewards,lengths,q_losses,pi_losses,temperature_loss,env_name,random_seed,episode,name):
+    
+    with open(f"./{name}_{env_name}-s{random_seed}-e{episode}-stat.pkl", 'wb') as f:
+        pickle.dump({"rewards" : rewards, "lengths": lengths,
+                        "pi_losses": pi_losses, "q_losses": q_losses,
+                        "temperature_loss":temperature_loss}, f)
+
+
+def reward_shaping(reward,info,player1):
+    #TODO: implement reward shaping from additional information
+    if player1:
+        return -reward
+    return reward
+
+def create_agent():
+    raise NotImplementedError()
+
+def run_sac_agent_in_env_modes(agent,mode,log_interval,save_interval,max_episodes,
+                                 max_timesteps,train_iter,random_seed,name=""):
+    torch.manual_seed(random_seed)
+    np.random.seed(random_seed)
+
+    if mode == "Defense":
+        env = h_env.HockeyEnv(mode=h_env.HockeyEnv.TRAIN_DEFENSE)
+    elif mode == "Attack":
+        env = h_env.HockeyEnv(mode=h_env.HockeyEnv.TRAIN_SHOOTING)
+    obs,info = env.reset()
+    
+    rewards = []
+    lengths = []
+    q_losses = []
+    policy_losses = []
+    temperature_losses = []
+
+    for episode in range(1,max_episodes+1):
+        ob, _info = env.reset()
+        total_reward=0
+        for t in range(max_timesteps):
+            done = False
+            a = agent.act(ob)
+            (ob_new,reward,done,trunc,_info) = env.step(a)
+            total_reward += reward
+            agent.store_transition((ob,a,reward,ob_new,done))
+            ob=ob_new
+            if done or trunc: break
+
+        q_loss,pi_loss,temperature_loss = agent.train(train_iter)
+        
+        q_losses.extend(q_loss)
+        policy_losses.extend(pi_loss)
+        temperature_losses.extend(temperature_loss)
+        rewards.append(total_reward)
+        lengths.append(t)
+
+        if episode % save_interval == 0:
+            print("########### Save checkpoint ################")
+            torch.save(agent.get_networks_states(),f'./{name}_{mode}-e{episode}-t{train_iter}-s{random_seed}.pth')
+            save_statistics(rewards,lengths,q_losses,policy_losses,temperature_losses,mode,random_seed,episode)
+
+        if episode % log_interval == 0:
+            avg_reward = np.mean(rewards[-log_interval:])
+            avg_length = int(np.mean(lengths[-log_interval:]))
+            print('Episode {} \t avg length: {} \t reward: {}'.format(episode, avg_length, avg_reward))
+        
+    save_statistics(rewards,lengths,q_losses,policy_losses,temperature_losses,mode,random_seed,episode)
+    
+
+def run_sac_agent_hockey_game(agent,opponent,mode,log_interval,save_interval,max_episodes,
+                                 max_timesteps,train_iter,random_seed,name="",show_both_logs=True):
+    torch.manual_seed(random_seed)
+    np.random.seed(random_seed)
+
+    player2 = agent
+
+    if mode == "easy":
+        env = h_env.HockeyEnv()
+        player1 = h_env.BasicOpponent(weak=True)
+    elif mode == "hard":
+        player1 = h_env.HockeyEnv(weak=False)
+    elif mode == "self":
+        player1 = opponent
+    
+        rewards1 = []
+        lengths1 = []
+        q_losses1 = []
+        policy_losses1 = []
+        temperature_losses1 = []
+        raise NotImplementedError("Implement self training")
+
+
+    obs,info = env.reset()
+    
+    rewards2 = []
+    lengths2 = []
+    q_losses2 = []
+    policy_losses2 = []
+    temperature_losses2 = []
+
+
+    for episode in range(1,max_episodes+1):
+        ob, _info = env.reset()
+        ob_player_2 = env.obs_agent_two()
+        total_reward=0
+        for t in range(max_timesteps):
+            done = False
+            action_player_1 = player1.act(ob)
+            action_player_2 = player2.act(ob_player_2)
+            (ob_new,reward,done,trunc,info) = env.step([action_player_1,action_player_2])
+            total_reward += reward
+            player2.store_transition((ob,action_player_2,reward,ob_new,done))
+            ob=ob_new
+            ob_player_2=env.obs_agent_two() 
+            if done or trunc: break
+
+        q_loss,pi_loss,temperature_loss = player2.train(train_iter)
+        
+        q_losses2.extend(q_loss)
+        policy_losses2.extend(pi_loss)
+        temperature_losses2.extend(temperature_loss)
+        rewards2.append(total_reward)
+        lengths2.append(t)
+
+        if mode=="self":
+            q_loss,pi_loss,temperature_loss = player1.train(train_iter)
+            
+            q_losses1.extend(q_loss)
+            policy_losses1.extend(pi_loss)
+            temperature_losses1.extend(temperature_loss)
+            rewards1.append(total_reward)
+            lengths1.append(t)
+
+        if episode % save_interval == 0:
+            print("########### Save checkpoint ################")
+            torch.save(player2.get_networks_states(),f'./{name}_{mode}-e{episode}-t{train_iter}-s{random_seed}-player2.pth')
+            if mode == "self":
+                torch.save(player2.get_networks_states(),f'./{name}_{mode}-e{episode}-t{train_iter}-s{random_seed}-player1.pth')
+                save_statistics(rewards1,lengths1,q_losses1,policy_losses1,temperature_losses1,mode,random_seed,episode)
+            save_statistics(rewards2,lengths2,q_losses2,policy_losses2,temperature_losses2,mode,random_seed,episode)
+
+        if episode % log_interval == 0:
+            avg_reward = np.mean(rewards2[-log_interval:])
+            avg_length = int(np.mean(lengths2[-log_interval:]))
+            print('Player2: Episode {} \t avg length: {} \t reward: {}'.format(episode, avg_length, avg_reward))
+            
+            if mode=="self" and show_both_logs:
+                avg_reward = np.mean(rewards1[-log_interval:])
+                avg_length = int(np.mean(lengths1[-log_interval:]))
+                print('Player1: Episode {} \t avg length: {} \t reward: {}'.format(episode, avg_length, avg_reward))
+
+        
+    save_statistics(rewards2,lengths2,q_losses2,policy_losses2,temperature_losses2,mode,random_seed,episode)
+    if mode =="self":
+        save_statistics(rewards1,lengths1,q_losses1,policy_losses1,temperature_losses1,mode,random_seed,episode)
+
+def main():
+    optParser = optparse.OptionParser()
+    optParser.add_option('-n', '--name',action='store', type='string',
+                         dest='name',default="SAC_run", help='Name for run')
+    optParser.add_option('-m', '--mode',action='store', type='string',
+                         dest='mode',default="Defense",
+                         help='Possible modes to play (default %default) options Attack/Defense/self')
+    optParser.add_option('-ti', '--train_iter',action='store',  type='int',
+                         dest='train_iter',default=32,
+                         help='number of training batches per episode (default %default)')
+    optParser.add_option('-l', '--lr',action='store',  type='float',
+                         dest='lr',default=0.0001,
+                         help='learning rate for actor/policy (default %default)')
+    optParser.add_option('-m', '--maxepisodes',action='store',  type='float',
+                         dest='max_episodes',default=2000, #change back to 2000
+                         help='number of episodes (default %default)')
+    optParser.add_option('-u', '--update',action='store',  type='float',
+                         dest='update_every',default=1,
+                         help='number of episodes between target network updates (default %default)')
+    optParser.add_option('-s', '--seed',action='store',  type='int',
+                         dest='seed',default=None,
+                         help='random seed (default %default)')
+    optParser.add_option('-a', '--agent',action='store',  type='string',
+                         dest='agent',default='SAC',
+                         help='Choose an Agent you wanna actiavte (default %default) Options: SAC/DSAC/DR3')
+    optParser.add_option('-o', '--opponent',action='store',  type='string',
+                         dest='opponent',default='SAC',
+                         help='Choose an opponent only in self (default %default) Options: SAC/DSAC/DR3')
+    optParser.add_option('-i', '--info',action='show',  type='string',
+                         help='This is the hockeytrain file from Team gradient descent also look at out github https://github.com/moemchen13/RL.git')
+    opts, args = optParser.parse_args()
+    ############## Hyperparameters ##############
+    run_name = opts.name
+    mode = opts.mode
+    agent = opts.agent
+    opponent = opts.opponent
+
+    render = False
+    log_interval = 20           # print avg reward in the interval
+    max_episodes = opts.max_episodes # max training episodes
+    max_timesteps = 2000         # max timesteps in one episode
+
+    train_iter = opts.train_iter      # update networks for given batched after every episode
+    lr  = opts.lr                # learning rate of DDPG policy
+    random_seed = opts.seed
+    save_interval=500
+    #############################################
+    
+    env = h_env.HockeyEnv()
+    
+    if agent == 'SAC':
+        agent = SAC_Agent(env.observation_space,env.action_space)
+    elif agent == 'DSAC':
+        agent = DSAC_Agent(env.observation_space,env.action_space) 
+    elif agent == 'DR3':
+        agent = DR3_Agent(env.observation_space,env.action_space)
+    
+    opponent = None
+    if mode == "self":
+        if opponent == 'SAC':
+            opponent = SAC_Agent(env.observation_space,env.action_space)
+        elif opponent == 'DSAC':
+            opponent = DSAC_Agent(env.observation_space,env.action_space) 
+        elif opponent == 'DR3':
+            opponent = DR3_Agent(env.observation_space,env.action_space)
+
+    if mode=="Defense" or mode=="Attack":
+        run_sac_agent_in_env_modes(agent,mode,log_interval,save_interval,max_episodes,
+                                 max_timesteps,train_iter,random_seed,name=run_name)
+    else:
+        run_sac_agent_hockey_game(agent,opponent,mode,log_interval,save_interval,max_episodes,
+                                 max_timesteps,train_iter,random_seed,run_name)
+
+if __name__ == '__main__':
+    main()
