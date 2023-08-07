e76569b9bd63a4ab971807d6f4cec7f50c0af21a
moemchen13 <85173797+moemchen13@users.noreply.github.com>
2023-08-07 19:49:25 +0200

    plot_changed to plot from multiple pickles

--- RL_Agent/SAC/plot_hockey.py
+++ RL_Agent/SAC/plot_hockey.py
@@ -3,12 +3,25 @@ import pickle
 import matplotlib.pyplot as plt
 import numpy as np
 
-with open("SAC_run_easy-s42-e4000-stat.pkl", 'rb') as f:
-    data = pickle.load(f)
-    rewards = np.asarray(data["rewards"])
-    q_losses =  np.asarray(data["q_losses"])
-    pi_losses  = np.asarray(data["pi_losses"])
-    temperature_losses = np.asarray(data["temperature_loss"])
+episode = 500
+max_episode = 10000
+all_rewards = []
+all_q_losses = []
+all_pi_losses = []
+all_temperature_losses = []
+
+while episode <= max_episode:
+    with open(f"SAC_run_easy-s42-e{episode}-stat.pkl", 'rb') as f:
+        data = pickle.load(f)
+        rewards = np.asarray(data["rewards"])
+        q_losses =  np.asarray(data["q_losses"])
+        pi_losses  = np.asarray(data["pi_losses"])
+        temperature_losses = np.asarray(data["temperature_loss"])
+        episode += 500
+        all_rewards.extend(rewards)
+        all_q_losses.extend(q_losses)
+        all_pi_losses.extend(pi_losses)
+        all_temperature_losses.extend(temperature_losses)
 
 
 def running_mean(x, N):
