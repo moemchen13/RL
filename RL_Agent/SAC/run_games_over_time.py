acbb61355c52272f00dd55713e14e1fead6f105e
moemchen13 <85173797+moemchen13@users.noreply.github.com>
2023-08-11 01:36:56 +0200

    now - before suffix

--- RL_Agent/SAC/run_games_over_time.py
+++ RL_Agent/SAC/run_games_over_time.py
@@ -70,7 +70,7 @@ def run_games(agent,opponent,step,points_in_plot,prefix,suffix,max_episode):
 
     for episode in range(step,points_in_plot*step+step,step):
         print(f'loaded agent for episode {episode}')
-        loading_file = prefix + str(episode) + suffix
+        loading_file = prefix + str(episode) + '-' + suffix
         load_Agent(agent,loading_file)
         win,loss,tie = play_hockey_game(agent,opponent,max_episode=max_episode)
         stats = np.vstack([stats,[win,loss,tie]])
@@ -83,17 +83,19 @@ def main():
     parser = argparse.ArgumentParser(prog='run_hockey')
     parser.add_argument('-f', '--file', dest='file',default="result.txt", help='file to save to')
     parser.add_argument('-p', '--prefix',dest='prefix',default='',help='prefix of the file')
-    parser.add_argument('-s', '--suffix',dest='suffix',default='-t32-s42-player.pth',help='file suffix (default %(default)s)')
-    parser.add_argument('-d', '--dsac',dest='dsac',action='store_true',help='is DSAC or not (default %(default)s)')
+    parser.add_argument('-s', '--suffix',dest='suffix',default='t32-s42-player.pth',help='file suffix (default %(default)s)')
+    parser.add_argument('-d', '--dsac',dest='dsac',action='store_true',help='is DSAC or not')
     parser.add_argument('-e', '--episodesteps',dest='episodesteps',default=500,help='stepsize (default %(default)s)')
-    parser.add_argument('-o', '--opponent_hard',dest='opponent',action='store_true',help='opponent hard (default %(default)s)')
+    parser.add_argument('-o', '--opponent_hard',dest='opponent',action='store_true',help='opponent hard')
     parser.add_argument('-i', '--points',dest='points',default=40,help='points time length the thing runs (default %(default)s)')
     parser.add_argument('-r', '--rounds',dest='rounds',default=1000,help='rounds to evaluate performance on (default %(default)s)')
     opts = parser.parse_args()
     ############## Hyperparameters ##############
+    alternating = opts.alternating
     weak = not bool(opts.opponent)
     prefix = opts.prefix
     suffix = opts.suffix
+    suffix = '-' + suffix
     is_DSAC = bool(opts.dsac)
     steps = int(opts.episodesteps)
     points_in_plot = int(opts.points)
